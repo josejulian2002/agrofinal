8ffb32714f0c6194eaa3ee145c8baf7781389e3a
miguelangelmz21 <55990584+miguelangelmz21@users.noreply.github.com>
2020-01-07 17:02:07 -0500

    Update UbicacionModel.py
    actualizacion2

--- proyecto_agro/Models/UbicacionModel.py
+++ proyecto_agro/Models/UbicacionModel.py
@@ -1,17 +1,19 @@
 from base_de_datos import bd
 
-class AlmacenModel(bd.Model):
+class Ubicacion(bd.Model):
     __tablename__="t_ubicacion"
     ubi_id = bd.Column("ubi_id",bd.Integer,primary_key=True)
+    ubi_ciud=bd.Column("ubi_ciud",bd.String(45))
     ubi_dir=bd.Column("ubi_dir",bd.String(200))
-    ubi_ciudad=bd.Column("ubi_ciudad",bd.String(45))
-    ubi_lat=bd.Column("ubi_lat",bd.DECIMAL(10,2))
-    ubi_lng=bd.Column("ubi_lng",bd.DECIMAL(10,2))
+    ubi_lat=bd.Column("ubi_lat",bd.DECIMAL(10,8))
+    ubi_lng=bd.Column("ubi_lng",bd.DECIMAL(10,8))
+    ubi_refer=bd.Column("ubi_refer",bd.TEXT)
     usu_id = bd.Column(bd.Integer, bd.ForeignKey('t_usuario.usu_id'), nullable=False)
 
-    def __init__(self,direccion,ciudad,latitud,longitud,usu_id):
+    def __init__(self,ciudad,direccion,latitud,longitud,referencia,usu_id):
+        self.ubi_ciud=ciudad
+        self.ubi_dir=direccion
         self.ubi_lat=latitud
         self.ubi_lng=longitud
-        self.ubi_ciudad=ciudad
-        self.ubi_dir=direccion
+        self.ubi_refer=referencia
         self.usu_id=usu_id
